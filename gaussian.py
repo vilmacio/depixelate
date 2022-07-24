b227d259592fe82dee42e46075046f25a231febd
vilmacio <vilmaciomoura@gmail.com>
2022-07-24 14:49:20 -0300

    fix(gaussian): scale improvement

--- gaussian.py
+++ gaussian.py
@@ -1,7 +1,9 @@
 import cv2
 
 def apply(image, weight = 5):
+    result = image
     ksizes = [1, 1]
+    max_applies = (weight // 2) + 1
 
     if (weight < 1 or weight > 10):
         raise Exception("Weight must be between 0 and 10. The received value was {}".format(weight))
@@ -11,6 +13,7 @@ def apply(image, weight = 5):
     if (ksizes[0] % 2 == 0):
         ksizes[0] = ksizes[1] = ksizes[1] - 1
 
-    result = cv2.GaussianBlur(image, ksizes, cv2.BORDER_DEFAULT)
+    for _ in range(max_applies):
+        result = cv2.GaussianBlur(result, ksizes, cv2.BORDER_DEFAULT)
 
     return result
