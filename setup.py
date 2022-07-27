4b507a63108cacffd70376b32b04f710ed26a2af
vilmacio <vilmaciomoura@gmail.com>
2022-07-27 12:50:35 -0300

    chore: create python setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="unpixalate",                     # This is the name of the package
+    version="0.0.1",                        # The initial release version
+    author="Vilmacio M. Silva",                     # Full name of the author
+    description="Unpixelate images",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["unpixelate"],             # Name of the python package
+    package_dir={'':'src'},     # Directory of the source code of the package
+    install_requires=[]                     # Install other dependencies if any
+)
