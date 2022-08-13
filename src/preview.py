2e34b57c8507358911c41dd76cfdfa90ce00a6e8
vilmacio <vilmaciomoura@gmail.com>
2022-08-13 09:51:04 -0300

    fix(package): update module name

--- src/preview.py
+++ src/preview.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy
 import shape
-import unpixelate
+import depixelate
 
 src = cv2.imread('assets/cat.jpg', cv2.IMREAD_UNCHANGED)
 mr_image = shape.resize(src, 600)
 
-result = unpixelate.apply(src, 8, 600)
+result = depixelate.apply(src, 8, 600)
 
 cv2.imshow('Depixelate Preview', numpy.hstack((mr_image, result)))
 cv2.waitKey(0)
