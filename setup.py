dc0295c0d357b3906cf09be04e82c7d50c0474ca
vilmacio <vilmaciomoura@gmail.com>
2022-08-18 15:10:43 -0300

    chore(package): increment fix change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="depixelate",                      # This is the name of the package
-    version="1.0.1",                        # The initial release version
+    version="1.0.2",                        # The initial release version
     author="Vilmacio M. Silva",                     # Full name of the author
     description="Depixelate images",
     long_description=long_description,      # Long description read from the the readme file
